c19cdbcfb5728d199cd73873921061a58219e170
casabuenangel <casabuenaangelrose1123@gmail.com>
2024-04-29 23:38:48 +0800

    save function

--- exercise_four/src/app/parent/parent.component.ts
+++ exercise_four/src/app/parent/parent.component.ts
@@ -12,6 +12,7 @@ export class ParentComponent {
   newMusic: Music = { id: 0, title: '', artist: '', genre: '', releaseYear: 0 };
   tempMusic: Music = { id: 0, title: '', artist: '', genre: '', releaseYear: 0 };
   isEditing: boolean = false;
+  lastIndex: number = -1;
 
   addMusicItem() {
     // Assign a unique ID to the new music item (you may need to adjust this logic)
@@ -23,11 +24,17 @@ export class ParentComponent {
     // Reset the tempMusic and newMusic objects to clear the form
     this.tempMusic = { id: 0, title: '', artist: '', genre: '', releaseYear: 0 };
     this.newMusic = { id: 0, title: '', artist: '', genre: '', releaseYear: 0 };
+    this.lastIndex = this.musicList.length - 1;
   }
 
   editMusicItem(musicItem: Music) {
     this.isEditing = true;
     Object.assign(this.newMusic, musicItem);
+  }
+
+  saveMusicItem() {
+    this.isEditing = false;
+    Object.assign(this.musicList[this.lastIndex], this.newMusic);
   }
 }
 
