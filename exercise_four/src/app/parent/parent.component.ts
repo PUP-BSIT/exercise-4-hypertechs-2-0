20c6e122d39e08ff730c540ee452fd78440c6a86
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-04-30 23:12:56 +0800

    trimn column length

--- exercise_four/src/app/parent/parent.component.ts
+++ exercise_four/src/app/parent/parent.component.ts
@@ -10,12 +10,17 @@ import { Music } from '../../model/music';
 
 export class ParentComponent {
   musicList: Music[] = [
-    { id: 1, title: 'Backstreet Freestyle', artist: 'Kendrick Lamar', genre: 'Hip-Hop', releaseYear: 2012 },
-    { id: 2, title: 'Only Ones Who Know', artist: 'Arctic Monkeys', genre: 'Alternative', releaseYear: 2007 },
-    { id: 1, title: 'Wicked Games', artist: 'The Weeknd', genre: 'R&B', releaseYear: 2011 },
+    { id: 1, title: 'Backstreet Freestyle', artist: 'Kendrick Lamar', 
+      genre: 'Hip-Hop', releaseYear: 2012 },
+    { id: 2, title: 'Only Ones Who Know', artist: 'Arctic Monkeys', 
+      genre: 'Alternative', releaseYear: 2007 },
+    { id: 1, title: 'Wicked Games', artist: 'The Weeknd', 
+      genre: 'R&B', releaseYear: 2011 },
   ];
-  newMusic: Music = { id: 0, title: '', artist: '', genre: '', releaseYear: 0 };
-  tempMusic: Music = { id: 0, title: '', artist: '', genre: '', releaseYear: 0 };
+  newMusic: Music = { id: 0, title: '', artist: '', genre: '', 
+    releaseYear: 0 };
+  tempMusic: Music = { id: 0, title: '', artist: '', genre: '', 
+    releaseYear: 0 };
   isEditing: boolean = false;
   lastIndex: number = -1;
 
@@ -27,7 +32,8 @@ export class ParentComponent {
   
     this.musicList.push(this.newMusic);
    
-    this.tempMusic = { id: 0, title: '', artist: '', genre: '', releaseYear: 0 };
+    this.tempMusic = { id: 0, title: '', artist: '', genre: '', 
+      releaseYear: 0 };
     this.newMusic = { id: 0, title: '', artist: '', genre: '', releaseYear: 0 };
     this.lastIndex = this.musicList.length - 1;
   }
